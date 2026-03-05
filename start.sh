fc676325f1fb46cb0cf760dba663ec959bdf5315
Scorpion <scorpion292439@gmail.com>
2026-03-05 11:00:56 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "=========================================="
-echo "🚀 BAŞLATILIYOR"
+echo "🚀 BAŞLATILIYOR - GİRİŞSİZ MOD"
 echo "PORT: $PORT"
 echo "=========================================="
 gunicorn --bind 0.0.0.0:$PORT --workers 1 --threads 2 server:app
